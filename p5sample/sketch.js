65c3280d22039b1e9b6f7279ae2b6946995a0965
Kelemen Mate <mate.kelemen009@gmail.com>
2017-11-26 16:28:24 +0200

    Basic circle moving left and right

--- p5sample/sketch.js
+++ p5sample/sketch.js
@@ -7,9 +7,9 @@ var xDir=1;
 
 function draw() {
   ellipse(x,50,80,80);
-  //if(x+40 >= windowWidth || x-40 <= 0){
-  //  xDir=-xDir;
-  //}
+  if(x+40 == windowWidth || x-40 == 0){
+    xDir=-xDir;
+  }
 
   x+=xDir;
 }
