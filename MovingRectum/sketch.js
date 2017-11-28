cb1588942fbadda217dbf03f52f845814e5b2bee
Kelemen Mate <mate.kelemen009@gmail.com>
2017-11-28 16:03:27 +0200

    i tried so hard and got so far

--- MovingRectum/sketch.js
+++ MovingRectum/sketch.js
@@ -1,22 +1,41 @@
-class Rect {
-  constructor(x,y,w,h) {
+function Rect(x,y,w,h) {
     this.x=x;
     this.y=y;
     this.w=w;
     this.h=h;
-  }
+    this.brightness = 0;
 
-  move(){
+  this.move=function() {
     this.x+=random(-5,5);
     this.y+=random(-5,5);
   }
 
-  show(){
+  this.show=function() {
+    rectMode(CENTER);
     stroke(255);
     strokeWeight(4);
-    rectMode(CENTER);
-    noFill();
+    fill(this.brightness, 125);
     rect(this.x,this.y,this.w,this.h);
+    strokeWeight(8);
+    stroke(255,0,0);
+    point(this.x,this.y);
+    point(this.x-this.w/2, this.y-this.h/2)
+    point(this.x+this.w/2, this.y+this.h/2);
+  }
+
+  this.isInside = function(mx,my){
+    return mx > this.x - this.w / 2 && mx < this.x + this.w / 2 && my > this.y - this.h / 2 && my < this.y + this.h / 2;
+  }
+
+  this.changeColor=function(color){
+    this.brightness = color;
+  }
+
+  this.intersectsRect = function(other){
+    if(this.x-this.w/2 > other.x+other.w/2 || other.x-other.w/2 > this.x+this.w/2) return false;
+    if(this.y-this.h/2 < other.y+other.h/2 || other.y-other.h/2 < this.y+this.h/2) return false;
+
+    return true;
   }
 }
 
@@ -26,22 +45,41 @@ function setup() {
   createCanvas(windowWidth,windowHeight);
   frameRate(60);
 
-  // for (let i = 0; i < 25; i++)
-  // {
-  //   rects[i] = new Rect(random(50,width-50),random(250,height-250),random(50,250),random(50,250));
-  // }
+  for (let i = 0; i < 2; i++)
+  {
+    let w = new Rect(random(50,width-50),random(250,height-250),random(50,250),random(50,250));
+    rects.push(w);
+  }
 }
 
 function draw() {
   background(51);
-  for (let i = 0; i < rects.length; i++) {
+  // for (let i = 0; i < rects.length; i++) {
+  //   for (let j = 0; j < rects.length; j++)
+  //   {
+  //     if(i!=j && rects[i].intersectsRect(rects[j])){
+  //       console.log("intersects");
+  //     }
+  //   }
+  //
+  //   rects[i].move();
+  //   rects[i].show();
+  // }
+  if(rects[0].intersectsRect(rects[1]) == true){
+    console.log("INTERSECTS");
+  }
+
+  for(let i = 0; i < rects.length; i++)
+  {
     rects[i].move();
     rects[i].show();
   }
 }
 
 function mousePressed() {
-  let r = random(50,250);
-  let b = new Rect(mouseX, mouseY, r, r);
-  rects.push(b);
+  for (let i = rects.length - 1; i >= 0; i--){
+    if(rects[i].isInside(mouseX, mouseY)){
+      rects.splice(i, 1);
+    }
+  }
 }
